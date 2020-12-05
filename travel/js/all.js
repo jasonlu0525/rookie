fbb068ff0137087641200b7ca66c20b7df008dd3
jasonlu0525 <51610162+jasonlu0525@users.noreply.github.com>
2020-12-05 20:19:03 +0800

    Add files via upload

--- travel/js/all.js
+++ travel/js/all.js
@@ -15,7 +15,7 @@ const data_filter = (get_condition) => { //篩選資料、分頁
     pagination.textContent = '';
     // ↓↓↓過濾資料
     for (let arr_index = 0; arr_index < ajax_data.result.records.length; arr_index++) {
-        if (get_condition !== ajax_data.result.records[arr_index].Zone && get_condition !== '全部景點' && get_condition !== '請選擇行政區') { // get_condition ==='全部景點'  ==>資料全抓；get_condition=== option的value屬性(XX區)
+        if (get_condition !== ajax_data.result.records[arr_index].Zone && get_condition !== '全部景點') { // get_condition ==='全部景點'  ==>資料全抓；get_condition=== option的value屬性(XX區)
             continue;
         } else {
             filter_data.push(ajax_data.result.records[arr_index]);
@@ -62,7 +62,6 @@ const data_filter = (get_condition) => { //篩選資料、分頁
     console.log(page_num);
     for (let page_num_index = 0; page_num_index < page_num.length; page_num_index++) {
         page_num[page_num_index].addEventListener('click', (e) => {
-            e.stopImmediatePropagation();
             for (let clear_attribute_index = 0; clear_attribute_index < page_num.length; clear_attribute_index++) {
                 page_num[clear_attribute_index].classList.remove('text-blue');
                 page_num[clear_attribute_index].removeAttribute('style');
@@ -107,7 +106,6 @@ const data_filter = (get_condition) => { //篩選資料、分頁
         clicked_pagination_num = clicked_pagination_num - 1;
     }, false);
     next_page.addEventListener('click', (e) => {
-        //    prev_page.classList.remove('text-gray-lighten');
         // ↓↓↓ 如果 沒有先按頁碼在按下一頁，這時clicked_pagination_num 是undefined ，於是強制將clicked_pagination_num 賦予 "初始直" 0，並且 重新render  畫面 (p.s if條件式只會執行1次)
         if (Boolean(clicked_pagination_num) === false && page_num.length > 1 && assign_data_perPage.length > 2) {
             clicked_pagination_num = 0;
@@ -146,46 +144,61 @@ const create_dom = (pickout_data, load_page_num_data) => {
         for (let onepage_data_Index = 0; onepage_data_Index < pickout_data[load_page_num_data].length; onepage_data_Index++) {
             let card = document.createElement('li');
             card.classList.add('card-items');
-            let card_ele = `
-                    <section class=" card-picture position-relative" style="background-image:url(${ pickout_data[load_page_num_data][onepage_data_Index].Picture1})"  alt="${pickout_data[load_page_num_data][onepage_data_Index].Picdescribe1}">
-        
+            card.setAttribute('data-order', onepage_data_Index);
+            let card_ele = `  
+                <section class=" card-picture position-relative"
+                    style="background-image:url(${ pickout_data[load_page_num_data][onepage_data_Index].Picture1})"
+                    alt="${pickout_data[load_page_num_data][onepage_data_Index].Picdescribe1}">
+
                         <div class="card-picture-tittle flex justify-content-between align-items-end">
                             <h3> ${ pickout_data[load_page_num_data][onepage_data_Index].Name}</h3>
                             <span class="text-bold">${ pickout_data[load_page_num_data][onepage_data_Index].Zone}</span>
                         </div>
-        
-                     </section>
-                    <section class="card-info position-relative flex">
-                    <div class="flex flex-dir-column justify-content-between align-items-center flex-shrink-0">
-                    <img src="image/icons_clock.png"  alt="開放時間">
-                    <img src="image/icons_pin.png"    alt="地址">
-                    <img src="image/icons_phone.png"     alt="連絡電話">
-                    </div>
+
+                </section>
+                <section class="card-info position-relative flex">
+                        <div class="flex flex-dir-column justify-content-between align-items-center flex-shrink-0" id="card-icons">
+                            <img src="image/icons_clock.png" alt="開放時間">
+                            <img src="image/icons_pin.png" alt="地址">
+                            <img src="image/icons_phone.png" alt="連絡電話">
+                        </div>
                         <ul class="flex justify-content-between align-items-end flex-wrap flex-grow-1">
-                           
-                                <li class=" flex align-items-center text-bold mb-13 flex-basis-100" >                        
-                                    <p class="text-bold ml-12 text-oneLine">${ pickout_data[load_page_num_data][onepage_data_Index].Opentime}</p>
-                                </li>                                
-                                <li class=" flex align-items-center text-bold mb-13 flex-basis-100" >                                  
-                                    <p class="text-bold ml-12 text-oneLine">${ pickout_data[load_page_num_data][onepage_data_Index].Add}</p>
-                                </li>
-                                <div class="flex justify-content-between flex-basis-100" >      
-                                <li class=" flex align-items-center text-bold  flex-shrink-0 "  style="margin-right:10px;">
-                               
-                                <a class="d-block text-bold ml-12 tex-origin" href="tel:${pickout_data[load_page_num_data][onepage_data_Index].Tel}">
-                                ${ pickout_data[load_page_num_data][onepage_data_Index].Tel}
-    </a>                
+
+                            <li class=" flex align-items-center text-bold mb-13 flex-basis-100">
+                                <p class="text-bold ml-12 text-oneLine" id="opentime">${ pickout_data[load_page_num_data][onepage_data_Index].Opentime}</p>
                             </li>
-                            <li class="flex align-items-center text-bold justify-content-end flex-basis-50" >
-                            <img src="image/icons_tag.png" alt="參觀門票">
-                        <p class="text-bold ml-12 text-oneLine" tittle="${  pickout_data[load_page_num_data][onepage_data_Index].Ticketinfo||'付費參觀'} ">${  pickout_data[load_page_num_data][onepage_data_Index].Ticketinfo||'付費參觀'}</p>
-                    </li>
-                                </div>    
+                            <li class=" flex align-items-center text-bold mb-13 flex-basis-100">
+                                <p class="text-bold ml-12 text-oneLine" id="location">${ pickout_data[load_page_num_data][onepage_data_Index].Add}</p>
+                            </li>
+                            <div class="flex justify-content-between flex-basis-100">
+                                <li class=" flex align-items-center text-bold  flex-shrink-0 " style="margin-right:10px;">
+                                    <a class="d-block text-bold ml-12 text-oneLine" href="tel:${pickout_data[load_page_num_data][onepage_data_Index].Tel}" id="phone">
+                                    ${pickout_data[load_page_num_data][onepage_data_Index].Tel}
+                                    </a>
+                                </li>
+                                <li class="flex align-items-center text-bold justify-content-end flex-basis-50">
+                                    <img src="image/icons_tag.png" alt="參觀門票">
+                                    <p class="text-bold ml-12 text-oneLine" tittle="${pickout_data[load_page_num_data][onepage_data_Index].Ticketinfo||'付費參觀'}" id="ticket-info">
+                                    ${pickout_data[load_page_num_data][onepage_data_Index].Ticketinfo||'付費參觀'}</p>
+                                </li>
+                            </div>
                         </ul>
-                    </section>
+                </section>
                  `;
             card.innerHTML = card_ele;
             list.appendChild(card);
+        }
+        let card_items = document.querySelectorAll('.card-items');
+        for (let card_items_index = 0; card_items_index < card_items.length; card_items_index++) {
+            card_items[card_items_index].addEventListener('click', () => {
+                let data_order_value = card_items[card_items_index].dataset.order;
+                document.querySelector(` #list li[data-order="${data_order_value}"] .card-picture-tittle h3`).classList.toggle('text-oneLine');;
+                console.log(data_order_value)
+                document.querySelector(`.card-items[data-order="${data_order_value}"] #opentime`).classList.toggle('text-oneLine');
+                document.querySelector(`.card-items[data-order="${data_order_value}"] #location`).classList.toggle('text-oneLine');
+                document.querySelector(`.card-items[data-order="${data_order_value}"] #phone`).classList.toggle('text-oneLine');
+                document.querySelector(`.card-items[data-order="${data_order_value}"] #ticket-info`).classList.toggle('text-oneLine');
+            }, false);
         }
     }
 }
@@ -209,16 +222,17 @@ window.addEventListener('scroll', () => {
     }
 });
 let select_option = document.querySelector('#select_area');
-select_option.addEventListener('change', (e) => {
-    e.preventDefault();
+select_option.addEventListener('input', (e) => {
     let option_value = e.target.value;
     let option_data_filter = data_filter(option_value);
     create_dom(option_data_filter, 0);
 }, false);
+select_option.addEventListener('click', (e) => {
+e.target.classList.toggle('arrow-up');
+}, false);
 let popular_btn = document.querySelectorAll('.area-btn');
 for (let area_btn_index = 0; area_btn_index < popular_btn.length; area_btn_index++) {
     popular_btn[area_btn_index].addEventListener('click', (e) => {
-        e.preventDefault();
         let btn_value = e.target.textContent;
         let section_data_filter = data_filter(btn_value);
         create_dom(section_data_filter, 0);
